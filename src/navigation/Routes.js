9ec225fd6b7373cc680af807c4536f4c02c144b0
Anonno <anonnodas97@gmail.com>
2024-09-04 11:42:58 +0600

    navigation complete

--- src/navigation/Routes.js
+++ src/navigation/Routes.js
@@ -4,14 +4,48 @@ import HomeScreen from "../screens/HomeScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import CartScreen from "../screens/CartScreen";
 import ProfileScreen from "../screens/ProfileScreen";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Routes = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home";
+          } else if (route.name === "Cart") {
+            iconName = focused ? "opencart" : "opencart";
+          } else if (route.name === "Profile") {
+            iconName = focused ? "user" : "user";
+          } else if (route.name === "Browse") {
+            iconName = focused ? "search" : "search";
+          } else if (route.name === "Favorites") {
+            iconName = focused ? "heart" : "heart";
+          }
+          // Return the icon component
+          return <FontAwesome5 name={iconName} size={size} color={color} />;
+        },
+        tabBarLabelStyle: { fontSize: 12 },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
       <Tab.Screen
         options={{ headerShown: false }}
         name="Home"
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        options={{ headerShown: false }}
+        name="Browse"
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        options={{ headerShown: false }}
+        name="Favorites"
         component={HomeScreen}
       />
       <Tab.Screen
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,84 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import React from "react";
 
 const HomeScreen = () => {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Text style={styles.name}>Hello Michael!!</Text>
+      <View
+        style={{
+          alignItems: "center",
+          flexDirection: "row",
+          gap: 20,
+          marginLeft: 20,
+        }}
+      >
+        <TouchableOpacity>
+          <Text>All</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Audio</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Mobile</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginRight: 20,
+        }}
+      >
+        <Text style={styles.deals}>Deals of the day</Text>
+        <TouchableOpacity>
+          <Text>See all</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          backgroundColor: "white",
+          padding: 20,
+          borderRadius: 12,
+          marginHorizontal: 20,
+          flexDirection: "row",
+          gap: 32,
+        }}
+      >
+        <Image
+          style={{ width: 96, height: 96 }}
+          source={require("../../assets/image/microphone.png")}
+        />
+        <View>
+          <Text style={{ color: "#868D94", fontWeight: 600 }}>Microphones</Text>
+          <View style={{ flexDirection: "row", gap: 12 }}>
+            <Text style={{ color: "tomato", fontWeight: 900 }}>$108.20</Text>
+            <Text
+              style={{ textDecorationLine: "line-through", color: "#B0B5B9s" }}
+            >
+              $199.99
+            </Text>
+          </View>
+          <Text style={{ fontWeight: 500, fontSize: 24 }}>RODE PodMic</Text>
+          <Text style={{ color: "#868D94" }}>
+            Dynamic microphone, Speaker {"\n"}microphone
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  name: { fontSize: 36, marginTop: 40, marginLeft: 20, fontWeight: "700" },
+
+  deals: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "black", // Example color
+    margin: 20,
+  },
+});
